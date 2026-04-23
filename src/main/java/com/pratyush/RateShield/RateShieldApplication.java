251f0066a9991313e657e6f0cffb37127c6e7fc1
Pratyush Sahu <sahupratyush27@gmail.com>
2026-04-23 19:43:15 +0530

    Initial commit

---
+++ src/main/java/com/pratyush/RateShield/RateShieldApplication.java
@@ -0,0 +1,13 @@
+package com.pratyush.RateShield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratyush/RateShield/RateShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratyush.RateShield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
